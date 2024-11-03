c600aa39a4889792b341340af9ef5e372a754935
nehemmy55 <n.ishimwe5@alustudent.com>
2024-11-03 10:38:09 -0800

    exception in py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end=' ')
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
             count += 1
-        except :
-            break
-    print() 
-    return count
+    except IndexError:
+        pass
+    finally:
+        print()
+        return count
 
-my_list = [1, "hello", 3.5, "world", 5]
-num_elements = safe_print_list(my_list, 7)
-print(f"\nNumber of elements printed: {num_elements}")
+
+if __name__ == "__main__":
+    my_list = [1, "me", 3, "turnup", 8]
+    result = safe_print_list(my_list, 7)
+    print("Number of elements printed:", result)
