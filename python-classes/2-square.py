b99d3d9ae6336169532e53f3b29ee76d2b470878
nehemmy55 <n.ishimwe5@alustudent.com>
2024-11-05 01:54:58 -0800

    raise exception in class

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Define aand declare class square"""
+"""Define and declare square"""
 
 
 class Square:
-    """Creates a class square
-    Private instance size
+    """Creates a class square.
+    Private instance  size
     """
 
     def __init__(self, size=0):
-        """Initialization """
+        """Initializes data."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be greater than 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
