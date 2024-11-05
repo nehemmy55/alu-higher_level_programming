068c8e59b0d4ba381331f5b16a95669fe65810d9
nehemmy55 <n.ishimwe5@alustudent.com>
2024-11-05 01:33:20 -0800

    defining class and declare

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-""" creation of class and definition of class """
+""" 
+creation of class and definition of class
+"""
 class square:
+    """
+    define empty class
+    """
     pass
