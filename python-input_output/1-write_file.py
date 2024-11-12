f9508dba4baccc037c164454eaa355408f681b92
nehemmy55 <n.ishimwe5@alustudent.com>
2024-11-12 10:42:35 -0800

    write file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-'''function that open and write file '''
+"""function that open and write file """
 
 def write_file(filename="", text=""):
-''' write file '''
+""" write file """
    with open(filename,'w+')as f:
       return f.write(text)
 
